544f1edf5af692b2d7abf1c49b242dc464ab656f
Mac HD <machd@Macs-MacBook-Pro.local>
2019-03-07 19:44:16 -0800

    adding bundle

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 
 const app = express();
 const proxy = require('http-proxy-middleware');
 const port = 3003;
 
-//app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: true}));
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(
   '/api/bookingInfo/', proxy({ target: 'http://localhost:3002', changeOrigin: true})
@@ -26,8 +23,15 @@ app.use(
 )
 
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.use(express.static(__dirname + '/public'));
+
+
+
 
 app.listen(port, () => {
   console.log(`I'm serving from http://localhost:${port}`);

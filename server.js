373cb667d8431ffaff631434f70dd4e41ffff3ca
Mac HD <machd@Macs-MacBook-Pro.local>
2019-03-07 16:31:58 -0800

    creating proxy server

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const proxy = require('http-proxy-middleware');
+const port = 3003;
 
 //app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: true}));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(bodyParser.urlencoded({extended: true}));
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+app.use(
+  '/api/bookingInfo/', proxy({ target: 'http://localhost:3002', changeOrigin: true})
+)
+
+app.use(
+  '/api/bookings/', proxy({ target: 'http://localhost:3002', changeOrigin: true})
+)
+
+app.use(
+  '/api/booking', proxy({target: 'http://localhost:3002', changeOrigin: true})
+)
+
+
+
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
